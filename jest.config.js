483a1e4699d1cfe498ec1124b02aa1a732a50469
Fernando Silva <fernando457829@gmail.com>
2020-08-24 16:10:07 -0300

    add jest for server

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { compilerOptions } = require('./server/tsconfig.json');
+
+module.exports = {
+  bail: 1,
+
+  collectCoverageFrom: ['<rootDir>/server/**/*.ts'],
+  coverageDirectory: '<rootDir>/coverage',
+  coverageReporters: ['text-summary', 'lcov'],
+
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/src' }),
+
+  preset: 'ts-jest',
+
+  testEnvironment: 'node',
+
+  testMatch: ['<rootDir>/**/*.test.ts'],
+};
