63ead7ba707056f79203b3e0f5307aa607061732
Fernando Silva <fernando457829@gmail.com>
2020-08-26 23:54:34 -0300

    fix property name

--- server/modules/users/entities/raws/IRawSession.ts
+++ server/modules/users/entities/raws/IRawSession.ts
@@ -2,5 +2,5 @@ import Session from '../Session';
 
 export default interface IRawSession extends Omit<Session, 'created_at' | 'expires_at'> {
   created_at: string;
-  updated_at: string;
+  expires_at: string;
 }
