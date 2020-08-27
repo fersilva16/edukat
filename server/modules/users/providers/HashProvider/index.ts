92e73b0054c4f5f40f09f50b3b617d4b129b6016
Fernando Silva <fernando457829@gmail.com>
2020-08-27 20:58:24 -0300

    add sha256 hash provider to container

--- server/modules/users/providers/HashProvider/index.ts
+++ server/modules/users/providers/HashProvider/index.ts
@@ -2,5 +2,8 @@ import { container } from 'tsyringe';
 
 import IHashProvider from './IHashProvider';
 import ArgonHashProvider from './implementations/ArgonHashProvider';
+import SHA256HashProvider from './implementations/SHA256HashProvider';
 
 container.registerSingleton<IHashProvider>('HashProvider', ArgonHashProvider);
+
+container.registerSingleton<IHashProvider>('SHA256HashProvider', SHA256HashProvider);
