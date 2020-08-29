6777e3b1e74a33d94576ae175028b80960fb4b5e
Fernando Silva <fernando457829@gmail.com>
2020-08-29 18:15:02 -0300

    wrap handle function

--- server/infra/http/routes/api.ts
+++ server/infra/http/routes/api.ts
@@ -1,11 +1,23 @@
-import { Router } from 'express';
+import {
+  Router,
+  Request,
+  Response,
+  NextFunction,
+} from 'express';
 
+import Exception from '~/exceptions/Exception';
 import Handler from '~/exceptions/Handler';
 
 const handler = new Handler();
 
 const apiRoutes = Router();
 
-apiRoutes.use(handler.handle);
+apiRoutes.use((
+  error: Exception | Error,
+  request: Request,
+  response: Response,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  next: NextFunction,
+) => handler.handle(error, request, response));
 
 export default apiRoutes;
