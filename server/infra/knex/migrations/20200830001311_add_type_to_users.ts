7b05342eba21ef1809c7631b48c86db24935d79e
Fernando Silva <fernando457829@gmail.com>
2020-08-30 00:20:31 -0300

    add type to users migration

---
+++ server/infra/knex/migrations/20200830001311_add_type_to_users.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex';
+
+const tableName = 'users';
+
+export async function up({ schema }: Knex): Promise<void> {
+  await schema.table(tableName, (table) => {
+    table.uuid('type_id').references('id').inTable('types').onDelete('CASCADE');
+  });
+}
+
+export async function down({ schema }: Knex): Promise<void> {
+  await schema.table(tableName, (table) => {
+    table.dropColumn('type_id');
+  });
+}
