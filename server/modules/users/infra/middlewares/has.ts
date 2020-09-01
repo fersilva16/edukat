ced0811cc91cdd731e7901af42e3c1b10180da65
Fernando Silva <fernando457829@gmail.com>
2020-08-31 21:00:10 -0300

    add has middleware

---
+++ server/modules/users/infra/middlewares/has.ts
@@ -0,0 +1,36 @@
+import { container } from 'tsyringe';
+import { Request, Response, NextFunction } from 'express';
+
+import Flags from '@users/dtos/Flags';
+import ITypeRepository from '@users/repositories/TypeRepository/ITypeRepository';
+import IPermissionProvider from '@users/providers/PermissionProvider/IPermissionProvider';
+
+import ResourceNotFoundException from '~/exceptions/ResourceNotFoundException';
+import MissingPermissionsException from '~/exceptions/MissingPermissionsException';
+
+const typeRepository = container.resolve<ITypeRepository>('TypeRepository');
+const permissionProvider = container.resolve<IPermissionProvider>('PermissionProvider');
+
+export default function has(...flags: (keyof typeof Flags)[]) {
+  return async (request: Request, response: Response, next: NextFunction): Promise<void> => {
+    const { user } = request;
+
+    const type = await typeRepository.findById(user.type_id);
+
+    if (!type) throw new ResourceNotFoundException('Type');
+
+    const permissions = Number(type.permissions);
+
+    const missingPermissions: (keyof typeof Flags)[] = [];
+
+    flags.forEach((name) => {
+      const permission = Flags[name];
+
+      if (!permissionProvider.has(permissions, permission)) missingPermissions.push(name);
+    });
+
+    if (missingPermissions.length) throw new MissingPermissionsException(missingPermissions);
+
+    next();
+  };
+}
