ffaded59bdd38fad89b77957b697f88af15cd1d9
Fernando Silva <fernando457829@gmail.com>
2020-09-01 14:57:53 -0300

    add unique for username and email

--- server/infra/knex/migrations/20200824215025_create_users.ts
+++ server/infra/knex/migrations/20200824215025_create_users.ts
@@ -7,8 +7,8 @@ export async function up({ schema }: Knex): Promise<void> {
     table.uuid('id').primary();
     table.string('firstname').notNullable();
     table.string('lastname').notNullable();
-    table.string('username', 15).notNullable();
-    table.string('email', 255).notNullable();
+    table.string('username', 15).notNullable().unique();
+    table.string('email', 255).notNullable().unique();
     table.string('password', 180).notNullable();
     table.timestamps();
   });
