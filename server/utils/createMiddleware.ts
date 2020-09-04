59dab8c7d85634f768978b710d9739191d37476e
Fernando Silva <fernando457829@gmail.com>
2020-09-03 22:29:10 -0300

    remove async

--- server/utils/createMiddleware.ts
+++ server/utils/createMiddleware.ts
@@ -2,7 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 type Middleware = (request: Request, response: Response, next: NextFunction) => Promise<void>;
 
-export default async function createMiddleware(middleware: Middleware) {
+export default function createMiddleware(middleware: Middleware) {
   return (
     request: Request,
     response: Response,
