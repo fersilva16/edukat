2009f6b04d20d23dbf6165fac91af6dbc8457a95
Fernando Silva <fernando457829@gmail.com>
2020-09-15 10:37:39 -0300

    feat(server): add is equal to validator

---
+++ server/utils/IsEqualTo.ts
@@ -0,0 +1,25 @@
+import { registerDecorator, ValidationOptions, ValidationArguments } from 'class-validator';
+
+export default function IsEqualTo(property: string, validationOptions?: ValidationOptions) {
+  return (object: Object, propertyName: string) => registerDecorator({
+    name: 'IsEqualTo',
+    target: object.constructor,
+    propertyName,
+    constraints: [property],
+    options: validationOptions,
+    validator: {
+      validate(value: any, args: ValidationArguments): boolean {
+        const [relatedPropertyName] = args.constraints;
+        const relatedValue = args.object[relatedPropertyName];
+
+        return typeof value === 'string' && typeof relatedValue === 'string' && value === relatedValue;
+      },
+
+      defaultMessage(args: ValidationArguments) {
+        const [relatedPropertyName] = args.constraints;
+
+        return `$property must match ${relatedPropertyName} exactly`;
+      },
+    },
+  });
+}
