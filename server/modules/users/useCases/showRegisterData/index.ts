f876525e2726b05f83c8a1e308ae03aa6f97eee0
Fernando Silva <fernando457829@gmail.com>
2020-09-18 17:57:32 -0300

    refactor(server): add dtos to container

--- server/modules/users/useCases/showRegisterData/index.ts
+++ server/modules/users/useCases/showRegisterData/index.ts
@@ -1,7 +1,9 @@
 import { container } from 'tsyringe';
 
 import ShowRegisterDataController from './ShowRegisterDataController';
+import ShowRegisterDataDTO from './ShowRegisterDataDTO';
 import ShowRegisterDataUseCase from './ShowRegisterDataUseCase';
 
 container.registerSingleton('ShowRegisterDataController', ShowRegisterDataController);
+container.register('ShowRegisterDataDTO', { useValue: ShowRegisterDataDTO });
 container.registerSingleton('ShowRegisterDataUseCase', ShowRegisterDataUseCase);
