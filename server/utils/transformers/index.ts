528fdad496d601777efd6436226e96967ec25383
Fernando Silva <fernando457829@gmail.com>
2020-09-20 09:03:57 -0300

    feat(server): add helper decorator

--- server/utils/transformers/index.ts
+++ server/utils/transformers/index.ts
@@ -2,6 +2,7 @@ export { default as Column } from './Column';
 export { default as DateColumn } from './DateColumn';
 export { default as SensitiveColumn } from './SensitiveColumn';
 export { default as Virtual } from './Virtual';
+export { default as Helper } from './Helper';
 export { default as Property } from './Property';
 
 export * as transform from './transform';
