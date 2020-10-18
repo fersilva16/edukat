54130017fbcd2636226da9b708626ca0c4769da0
Fernando Silva <fernando457829@gmail.com>
2020-10-18 17:25:54 +0000

    feat(server): add max length to refresh_token column

--- server/infra/knex/migrations/20200826225330_create_sessions.ts
+++ server/infra/knex/migrations/20200826225330_create_sessions.ts
@@ -8,7 +8,7 @@ export async function up({ schema }: Knex): Promise<void> {
   await schema.createTable(tableName, (table) => {
     table.string('id', appConfig.idLength).primary();
     table.string('token', 64).notNullable();
-    table.string('refresh_token').notNullable();
+    table.string('refresh_token', 64).notNullable();
     table.string('user_id').references('id').inTable('users').onDelete('CASCADE');
     table.timestamp('created_at', { useTz: true }).notNullable();
     table.timestamp('expires_at', { useTz: true }).nullable();
