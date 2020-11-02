dc831d24bb3d2c155f60439f3ad2bb43b589afe8
Fernando Silva <fernando457829@gmail.com>
2020-11-02 05:23:45 +0000

    refactor(server): fix replace function

--- server/utils/transformRepositoryDTO.ts
+++ server/utils/transformRepositoryDTO.ts
@@ -3,7 +3,7 @@ import { DateTime } from 'luxon';
 export default function transformRepositoryDTO<T extends Object>(object: Object): T {
   return Object.fromEntries(
     Object.entries(object).map(([key, value]) => [
-      key.replace(/([a-z])([A-Z])/g, ([before, after]) => `${before}_${after}`).toLowerCase(),
+      key.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase(),
       DateTime.isDateTime(value) ? value.toISO()! : value,
     ]),
   ) as T;
